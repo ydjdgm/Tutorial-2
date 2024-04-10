3c5445566f247d01ca188a0e216388bf6d252511
ydjdgm <tonytony060720@gmail.com>
2024-04-10 18:10:31 +0200

    Day2
    코드 깔끔하게 정리

--- Tutorial 2/Assets/Scripts/Player.cs
+++ Tutorial 2/Assets/Scripts/Player.cs
@@ -6,99 +6,108 @@ using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody rb;
+    public int gameStatus = 2;//0 = 주금, 1 = 클리어, 2 = 진행 중
 
-    public int GameStatue { get; set;} //0은 클리어 실패, 1은 클리어, 2는 플레이 중
-    
-    public GameObject respawn;
-    public GameObject[] items;
-    public float timeLimit;
     public float moveSpeed;
     public float jumpPower;
-    public int gotItem;
+
     public int maxItem;
+    public int gotItem;
 
-    bool isJump = true;
+    public GameObject respawn;
+    public GameObject[] items;
 
+    Rigidbody rb;
 
-    private void Awake()
+    float hAxis;
+    float vAxis;
+
+    bool jumpDown;
+    bool rDown;
+
+    public bool isJump;
+
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        GameStatue = 2;
     }
-
     private void FixedUpdate()
     {
-        float hAxis = Input.GetAxis("Horizontal");
-        float vAxis = Input.GetAxis("Vertical");
-        //if(isJump != false) { }
-        rb.AddForce(new Vector3(hAxis * moveSpeed * Time.deltaTime, 0, vAxis * moveSpeed * Time.deltaTime), ForceMode.Impulse);
-        //transform.position = new Vector3(hAxis * moveSpeed * Time.deltaTime, 0, vAxis * moveSpeed * Time.deltaTime);
+        GetInputForFixedUpdate();
+        Move();
     }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            SelfRespawn();
-        }
-        if (Input.GetButtonDown("Jump") && isJump == false)
-        {
-            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
-            isJump = true;
-        }
-        if(transform.position.y < -15 || timeLimit <= 0f)
-        {
-            GameStatue = 0;
-        }
-        if(GameStatue == 1)
-        {
-            //게임 클리어 로직
-        }else if(GameStatue == 0)
-        {
-            for (int i = 0; i < maxItem; i++)//으아아아아
-            {
-                items[i].SetActive(true);
-            }
-            transform.position = respawn.transform.position;
-            rb.constraints = RigidbodyConstraints.FreezePosition;
-            Invoke("SetGameStatus2", 0.5f);
-        }else if(GameStatue == 2)
-        {
-            rb.constraints = RigidbodyConstraints.None;
-        }
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Floor")
+        if (other.tag == "Floor")
         {
             isJump = false;
-        }
-        if (other.tag == "Finish" && gotItem >= maxItem)
+        }else if (other.tag == "Finish" && gotItem >= maxItem)
         {
-            GameStatue = 1;
-        }
-        if(other.tag == "Item")
+            gameStatus = 1;
+        }else if(other.tag == "Item")
         {
             gotItem++;
         }
     }
-
-    private void OnTriggerExit(Collider other)
+    private void Update()
     {
-        if(other.tag == "Floor")
+        GetInput();
+        Jump();
+        SetGameStatus0();
+        if(gameStatus == 0)
         {
+            Respawn();
+        }
+        Win();//미완성 함수
+    }
+    void GetInputForFixedUpdate()
+    {
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis ("Vertical");
+    }
+    void GetInput()
+    {
+        rDown = Input.GetKeyDown(KeyCode.R);
+        jumpDown = Input.GetButtonDown("Jump");
+    }
+    void Move()
+    {
+        rb.AddForce(new Vector3(hAxis * moveSpeed, 0, vAxis * moveSpeed), ForceMode.Impulse);
+    }
+    void Jump()
+    {
+        if (jumpDown && !isJump)
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
             isJump = true;
+        }
+    }
+    void SetGameStatus0()//Set1은 OnTriggerEnter에 Set2는 
+    {
+        if(transform.position.y < -15 || rDown)
+        {
+            gameStatus = 0;
         }
     }
     void SetGameStatus2()
     {
-        GameStatue = 2;
+        gameStatus = 2;
+        rb.constraints = RigidbodyConstraints.None;
+        gotItem = 0;
+        for (int i = 0; i < maxItem; i++)
+        {
+            items[i].gameObject.SetActive(true);
+        }
     }
-
-    public void SelfRespawn()
+    void Win()
     {
-        GameStatue = 0;
+        //클리어 시 로직
+    }
+    public void Respawn()
+    {
+        transform.position = respawn.transform.position;
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        Invoke("SetGameStatus2", 0.5f);
     }
 }
