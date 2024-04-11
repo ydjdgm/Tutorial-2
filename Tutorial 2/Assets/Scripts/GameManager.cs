e4ac855a404af4436d5b411226659f2415943a07
ydjdgm <tonytony060720@gmail.com>
2024-04-11 07:05:41 +0200

    day3
    dd

--- Tutorial 2/Assets/Scripts/GameManager.cs
+++ Tutorial 2/Assets/Scripts/GameManager.cs
@@ -7,14 +7,35 @@ public class GameManager : MonoBehaviour
 {
     public int maxItem;
     public int stage;
+    public double timeLimit;
+    public Text stageText;
+    public Text timeLimitText;
     public Text gotItemText;
     public Text maxItemText;
     private void Awake()
     {
         maxItemText.text = "/ "+maxItem;
     }
+    void StageTextUpdate()
+    {
+        stageText.text = "Stage"+stage.ToString();
+        timeLimit -= Time.deltaTime;
+    }
     public void GotItemTextUpdate(int gotItem)
     {
         gotItemText.text = gotItem.ToString();
+    }
+    private void Update()
+    {
+        StageTextUpdate();
+        TimeLimitTextUpdate();
+    }
+    void TimeLimitTextUpdate()
+    {
+        if (timeLimit >= 5f)
+        {
+
+        }
+        timeLimitText.text = timeLimit.ToString();
     }
 }
